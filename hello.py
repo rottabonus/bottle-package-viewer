64198942f8fdea5705c4596cc677c102cf429a1c
rottabonus <feqwin@gmail.com>
2020-08-16 08:10:26 +0300

    added gitignore for virtual environment

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
 from bottle import route, run, template
 import package_reader
 
+count_of_packages = package_reader.return_packages_list()
 
 @route('/hello')
 def hello():
-    count_of_packages = package_reader.return_packages_list()
     return template('hello_template', count_of_packages=count_of_packages)
 
 run(host='0.0.0.0', port=3000, debug=True)
